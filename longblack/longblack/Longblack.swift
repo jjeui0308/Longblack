96a416ab542127af4d16921ee80fab33d0bc8422
Justin Ji <jjeui0308@gmail.com>
2019-05-06 17:47:14 +0900

    initial commit

---
+++ longblack/longblack/Longblack.swift
@@ -0,0 +1,18 @@
+//
+//  Longblack.swift
+//  longblack
+//
+//  Created by Justin Ji on 06/05/2019.
+//  Copyright © 2019 tlsolution. All rights reserved.
+//
+
+import Foundation
+
+class Longblack {
+    
+    private init() {}
+    
+    static func sayHello() {
+        print("Hello 😜")
+    }
+}
---
+++ longblack/longblack/longblack.h
@@ -0,0 +1,19 @@
+//
+//  longblack.h
+//  longblack
+//
+//  Created by Justin Ji on 06/05/2019.
+//  Copyright © 2019 tlsolution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for longblack.
+FOUNDATION_EXPORT double longblackVersionNumber;
+
+//! Project version string for longblack.
+FOUNDATION_EXPORT const unsigned char longblackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <longblack/PublicHeader.h>
+
+
